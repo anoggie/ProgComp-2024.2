54d4cd83face624031ce8e4389ebfcb84733f593
anoggie <aniellynf12@gmail.com>
2024-12-22 18:25:08 -0300

    first commit

---
+++ Unidade01-Avaliacao02/q2.py
@@ -0,0 +1,29 @@
+# questão 2 - Quantidade de palíndromos entre 10 e 100000
+# by Anielly & Israel
+
+contador = 0 # inicializa o contador de palíndromos
+
+for i in range(10, 100001): # percorre os números de 10 a 100000
+    original = i # guarda o valor original
+    reverso = 0 # guarda o valor reverso
+
+    while i > 0: # inverte o número
+        ultimo_digito = i % 10 # pega o último dígito
+        reverso = reverso * 10 + ultimo_digito # constrói o número reversamente
+        i = i // 10 # remove o último dígito
+
+    if original == reverso: # verifica se o número é palíndromo
+        contador += 1 # incrementa ao contador
+
+print(contador)
+
+'''
+NOTA DOS DEVS: Essa questão poderia ter sido feita de duas formas, uma onde definiriamos variáveis para não ficar mexendo nos laços:
+
+    inicial = 10
+    teto = 100000 + 1 (o + 1 serve para não dar errado no range)
+
+    for i in range(inicial, teto):
+    
+A segunda maneira, é a que está feita na questão, que definimos o range diretamente no laço.
+'''
---
+++ Unidade01-Avaliacao02/q3.py
@@ -0,0 +1,21 @@
+# questão 3 - Pares d eprimos ímpares consecutivos < 1000000
+# by Anielly & Israel
+
+limite = 1000000 # limite superior
+primeiro_impar = 3 # inicializa o primeiro ímpar
+pares = 0
+
+for n in range(5, limite, 2):
+    primo = True # assume que n é primo de primeira
+
+    for i in range(2, int(n**0.5) + 1): # verifica se n é primo
+        if n % i == 0:
+            primo = False # se n for divisível por i, n não é primo
+            break
+
+    if primo:
+        if n - primeiro_impar == 2: # verifica se a diferença entre os ímpares é 2
+            pares += 1 # incrementa a quantidade de pares encontrados
+        primeiro_impar = n
+
+print(f'Quantidade de pares: {pares}')
